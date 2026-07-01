7ef6ce09b1b9040d260f14ba1729dd6782677f04
fioravantefelipe <felipe.fioravante@aluno.senai.br>
2026-06-30 23:04:49 -0300

    Aula 7

---
+++ script.js
@@ -0,0 +1,43 @@
+const precoIngresso = 100;
+const form = document.getElementById("form-inscricao");
+
+function processarInscricao(nome, idade, possuiCupom){
+    let precoFinal = precoIngresso;
+
+    if(idade < 16){
+        console.log("Inscricao Bloqueada: "+nome+" não possui idade minima")
+    } else {
+        if(possuiCupom) {
+            precoFinal = precoFinal - 20;
+        }
+        console.log("Inscricao Confirmada para "+nome+"!");
+        console.log("Valor a pagar: R$"+precoFinal);
+    }
+}
+
+function simularVendaDeIngrssos(){
+    for(let i = 5; i > 0; i--){
+        console.log("Corra! Faltam apenas "+i+" vagas para o TechDay 2026");
+    }
+    console.log("Vagas Encerradas!");
+}
+
+form.addEventListener(
+    "submit",
+    function(event){
+        event.preventDefault();
+        console.log("Formulario interceptado com sucesso!")
+        const valorNome = document.getElementById("nome").value;
+        const valorEmail = document.getElementById("email").value;
+
+        if(valorNome.trim() === ""){
+            alert("Por favor, preencha o seu nome!");
+        }
+        if(valorEmail.trim() === ""){
+            alert("Por favor, preencha o seu email");
+        }
+        localStorage.setItem("techday_nome", "valorNome");
+        localStorage.setItem("techday_email","valorEmail");
+
+    }
+)
